68316d6d89d7a1a6a0056d4f01cf35a74d482cbc
Mariana Vorotniak <mvoro@softserveinc.com>
2018-12-26 12:30:59 +0200

    first commit

--- src/main/java/GitDemo.java
+++ src/main/java/GitDemo.java
@@ -1,8 +1,13 @@
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.DoubleStream;
 
 public class GitDemo {
 
     public static void main(String args[]) {
-
+        List<Double> list = DoubleStream.generate(() -> Math.random() * 10).limit(5).boxed().collect(Collectors.toList());
+        System.out.println("The random numbers: ");
+        list.stream().forEach(System.out::println);
     }
-    
+
 }
