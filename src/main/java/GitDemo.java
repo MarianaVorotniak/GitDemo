71385dc422c1110fd95800e00002076b56741663
Dima Kirbaba <dkirbaba@homeaway.com>
2018-12-26 13:40:14 +0200

    [Ticket Number] some external commit

--- src/main/java/GitDemo.java
+++ src/main/java/GitDemo.java
@@ -7,7 +7,8 @@ public class GitDemo {
     public static void main(String args[]) {
         List<Double> list = DoubleStream.generate(() -> Math.random() * 10).limit(5).boxed().collect(Collectors.toList());
         System.out.println("The random numbers: ");
-        list.stream().forEach(System.out::println);
+        list.stream().filter(i -> i<4);
+
     }
 
 }
